73d3f340a32a7a2e24fce1252ae606fe04d0fdb7
Vipul Rustagi <vipulrustagi11@gmail.com>
2018-01-22 01:05:29 +0530

    Implementing Stacks using Linked lists.

---
+++ Stacks/Using Linked Lists/StackUsingLinkedList.java
@@ -0,0 +1,27 @@
+package stackandqueues.stackusingLinkedlist;
+
+public class StackUsingLinkedList {
+	private Node top ;
+	private int size;
+	public StackUsingLinkedList() {
+		top =null;
+		size=0;
+	}
+	
+	push(){
+	size++;	
+	}
+	pop(){
+		size--;
+	}
+	
+	isEmpty(){
+		
+	}
+	size(){
+		return size;
+	}
+	
+	
+
+}
